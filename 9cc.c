5d714e84918172f102e351c8e4832963d4dad31c
mrsekut <k.cloudspider@gmail.com>
2019-04-04 00:38:45 +0900

    [Add] mul and div

--- 9cc.c
+++ 9cc.c
@@ -1,13 +1,25 @@
+#include <assert.h>
 #include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Tokenizer
 enum
 {
     TK_NUM = 256,
     TK_EOF,
 };
+
+enum
+{
+    ND_NUM = 256,
+};
+
+// Recursive-descendent parser
+
+int pos = 0;
 
 typedef struct
 {
@@ -15,6 +27,14 @@ typedef struct
     int val;
     char *input;
 } Token;
+
+typedef struct
+{
+    int type;
+    struct Node *lhs;
+    struct Node *rhs;
+    int val;
+} Node;
 
 Token tokens[100];
 
@@ -29,7 +49,7 @@ void tokenize(char *p)
             continue;
         }
 
-        if (*p == '+' || *p == '-')
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/')
         {
             tokens[i].type = *p;
             tokens[i].input = p;
@@ -53,6 +73,110 @@ void tokenize(char *p)
     tokens[i].input = p;
 }
 
+Node *new_node(int type, Node *lhs, Node *rhs)
+{
+    Node *node = malloc(sizeof(Node));
+    node->type = type;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = malloc(sizeof(Node));
+    node->type = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+int consume(int type)
+{
+    if (tokens[pos].type != type)
+        return 0;
+    pos++;
+    return 1;
+}
+
+// TODO:
+Node *term();
+
+Node *mul()
+{
+    Node *node = term();
+
+    for (;;)
+    {
+        if (consume('*'))
+            node = new_node('*', node, term());
+        else if (consume('/'))
+            node = new_node('/', node, term());
+        else
+            return node;
+    }
+}
+Node *add()
+{
+    Node *node = mul();
+
+    for (;;)
+    {
+        if (consume('+'))
+            node = new_node('+', node, mul());
+        else if (consume('-'))
+            node = new_node('-', node, mul());
+        else
+            return node;
+    }
+}
+Node *term()
+{
+    if (consume('('))
+    {
+        Node *node = add();
+        if (!consume(')'))
+            error("対応する括弧がありません: %s", tokens[pos].input);
+        return node;
+    }
+    if (tokens[pos].type == TK_NUM)
+        return new_node_num(tokens[pos++].val);
+
+    error("数値でも開き括弧でもないトークンです: %s", tokens[pos].input);
+}
+
+void gen(Node *node)
+{
+    if (node->type == ND_NUM)
+    {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->type)
+    {
+
+    case '+':
+        printf("    add rax, rdi\n");
+        break;
+    case '-':
+        printf("    sub rax, rdi\n");
+        break;
+    case '*':
+        printf("    mul rdi\n");
+        break;
+    case '/':
+        printf("    mov rdx, 0\n");
+        printf("    div rdi\n");
+    }
+    printf("    push rax\n");
+}
+
 void error(int i)
 {
     fprintf(stderr, "予期しないトークンです: %s\n", tokens[i].input);
@@ -68,41 +192,15 @@ int main(int argc, char **argv)
     }
 
     tokenize(argv[1]);
+    Node *node = add();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    if (tokens[0].type != TK_NUM)
-        error(0);
+    gen(node);
 
-    printf("    mov rax, %d\n", tokens[0].val);
-
-    int i = 1;
-    while (tokens[i].type != TK_EOF)
-    {
-        if (tokens[i].type == '+')
-        {
-            i++;
-            if (tokens[i].type != TK_NUM)
-                error(i);
-            printf("    add rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        if (tokens[i].type == '-')
-        {
-            i++;
-            if (tokens[i].type != TK_NUM)
-                error(i);
-            printf("    add rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-        error(i);
-    }
-
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
